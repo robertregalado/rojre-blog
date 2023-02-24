223fa1ad9c90f029a5a8ab758d69a8764ed66507
Robert J. Regalado <rjregalado1979@gmail.com>
2023-02-25 01:50:30 +0800

    add composables folder

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>Loading...</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { ref } from 'vue'
+import getPosts from '../composables/getPosts'
+// component imports
+import PostList from '../components/PostList.vue'
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  name: 'Home',
+  components: { PostList },
+  setup() { 
+    const { posts, error, load } = getPosts()
+    load()
+    
+    return { posts, error }
+  },
 }
 </script>
+
